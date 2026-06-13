77933739ee4924c7fc9381a03e6fb2c77b59204c
Edoardo Oranger <edo.oranger@gmail.com>
2026-06-13 13:21:31 +0200

    fix: ssh.dockerfile and entrypoint.sh
    * Fix 'ssh.dockerfile' definition for user password authentication
    * Updated configuration of 'entrypoint.sh' for logs

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOG_FILE_PATH="/var/log/sshd.log"
+
 function ssh_server_intialization ()
 {
     local PERMIT_ROOT_LOGIN="no"
@@ -7,17 +9,22 @@ function ssh_server_intialization ()
     local PUBKEY_AUTHENTICATION="yes"
     local AUTHORIZED_KEYS_FILE_PATH="/home/testuser/.ssh/authorized_keys"
     local SSH_CONFIG_FILE="/etc/ssh/sshd_config"
+    local LOG_LEVEL="DEBUG3"
+    local SYS_LOG_FACILITY="AUTH"
 
     printf "PermitRootLogin ${PERMIT_ROOT_LOGIN}\n" >> ${SSH_CONFIG_FILE}
     printf "PasswordAuthentication ${PASSWORD_AUTHENTICATION}\n" >> ${SSH_CONFIG_FILE}
     printf "PubkeyAuthentication ${PUBKEY_AUTHENTICATION}\n" >> ${SSH_CONFIG_FILE}
     printf "AuthorizedKeysFile ${AUTHORIZED_KEYS_FILE_PATH}\n" >> ${SSH_CONFIG_FILE}
+    printf "LogLevel ${LOG_LEVEL}\n" >> ${SSH_CONFIG_FILE}
+    printf "SyslogFacility ${SYS_LOG_FACILITY}\n" >> ${SSH_CONFIG_FILE}
 }
 
 printf ">>> SSH server initialization ...\n"
 
 ssh_server_intialization
-/usr/sbin/sshd
+ssh-keygen -A
+/usr/sbin/sshd -E ${LOG_FILE_PATH}
 
 printf ">>> SSH server initialized successfully!\n"
 
--- ssh.dockerfile
+++ ssh.dockerfile
@@ -1,17 +1,21 @@
 FROM alpine:3.20
 
-RUN adduser -D -s /bin/sh testuser
+RUN adduser -D -s /bin/sh testuser && \
+    echo "testuser:changeme" | chpasswd
 
-RUN apk add --no-cache openssh
+RUN apk add --no-cache openssh dos2unix bash
 
 RUN mkdir /home/testuser/.ssh && \
-    chmod 766 /home/testuser/.ssh && \
+    chmod 700 /home/testuser/.ssh && \
     chown -R testuser:testuser /home/testuser/.ssh
 
 COPY src/entrypoint.sh /home/testuser/app/entrypoint.sh
-COPY ssh_key /home/testuser/.ssh/authorized_keys
+COPY ssh_key.pub /home/testuser/.ssh/authorized_keys
 
-RUN chmod +x /home/testuser/app/entrypoint.sh && \
+RUN chmod 600 /home/testuser/.ssh/authorized_keys && \
+    chown testuser:testuser /home/testuser/.ssh/authorized_keys && \
+    dos2unix /home/testuser/app/entrypoint.sh && \
+    chmod 766 /home/testuser/app/entrypoint.sh && \
     chown -R testuser:testuser /home/testuser/app
 
 EXPOSE 22
