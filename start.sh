ce9be8b7f667d66d24d2e9740edcece7c30d6799
Edoardo Oranger <edo.oranger@gmail.com>
2026-06-27 12:27:29 +0200

    chore: Removed Useless Comments

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 
 set -eou pipefail
 
-# Build of the application
 docker compose --file docker-compose.ssh.yaml build --progress plain 2>&1 | tee logs/build.log
-
-# Running the application
 docker compose --file docker-compose.ssh.yaml up -d
